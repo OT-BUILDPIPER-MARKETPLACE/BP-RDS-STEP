e107bf27e3e7f22dde8261a082e0c83c81141ec1
balpreetbanga18 <99896974+balpreetbanga18@users.noreply.github.com>
2023-06-27 19:20:11 +0530

    Update main.tf

--- RDS/main.tf
+++ RDS/main.tf
@@ -4,22 +4,22 @@ module "mysql" {
   database_subnet_ids      = [data.terraform_remote_state.remote.outputs.db-subnet-ids[0], data.terraform_remote_state.remote.outputs.db-subnet-ids[1]]
   username                 = var.username
   password                 = var.password
-  engine_name = var.engine_name
-  identifier   = var.identifier
+  engine_name              = var.engine_name
+  identifier               = var.identifier
   instance_class           = var.instance_class
   allocated_storage        = var.allocated_storage
 
-database_security_groups = [module.dev_rds_sg.sg_id]
+database_security_groups = [module.rds_sg.sg_id]
 
 
 }
 
-module "dev_rds_sg" {
+module "rds_sg" {
   source                             = "OT-CLOUD-KIT/security-groups/aws"
   version                            = "1.0.0"
   enable_whitelist_ip                = true
   enable_source_security_group_entry = false
-  name_sg                            = "non-prod-valuefy-rds-mysql-sg"
+  name_sg                            = "var.sg_name"
   vpc_id                             = data.terraform_remote_state.remote.outputs.vpc_id
   ingress_rule = {
     rules = {
